209fbe649b272073f33eb851f69b6a6d18ef477f
Gaston Caminiti <gastoncaminiti@gmail.com>
2021-10-18 13:23:19 -0300

    Commit 1 [PRACTICA EN VIVO]
    Control de Jugador.
    Temporizador.
    Empleo de Inputs.

--- CLASE 6 - INPUT/Assets/Scripts/EnemyController.cs
+++ CLASE 6 - INPUT/Assets/Scripts/EnemyController.cs
@@ -5,24 +5,68 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speedEnemy = 5.0f;
+    public float liveEnemy = 7.0f;
+    bool isForward = true;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("EN EL FRAME INICIAL ENEMY...");
         Debug.DrawLine(transform.position, new Vector3(5, 0, 0), Color.red, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(speedEnemy * Vector3.right * Time.deltaTime);
-        //transform.position += new Vector3(speedEnemy, 0, 0) * Time.deltaTime;
-        //transform.Translate(speedEnemy * Time.deltaTime * Vector3.right);
-        MoveEnemy();
-    }
+        /* IF PRIMER EJEMPLO
+        if(transform.position.x < 20f) { 
+            MoveEnemy();
+        }
+        */
+        /* IF SEGUNDO EJEMPLO CON ERROR
+        if (transform.position.x < 20f)
+        {
+            MoveEnemy(Vector3.forward);
+        }
+        else
+        {
+            MoveEnemy(Vector3.back);
+        }
+        */
+        //IF TERCER EJEMPLO
+        if (isForward)
+        {
+            MoveEnemy(Vector3.forward);
+        }
+        else
+        {
+            MoveEnemy(Vector3.back);
+        }
 
+        if (transform.position.x < 0f && !isForward)
+        {
+            isForward = true;
+        }
+
+        if (transform.position.x > 20f && isForward)
+        {
+            isForward = false;
+        }
+
+        liveEnemy -= Time.deltaTime;
+
+        if (liveEnemy <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*
     private void MoveEnemy()
     {
         transform.Translate(speedEnemy * Time.deltaTime * Vector3.forward);
+    }
+    */
+    //EJEMPLO METODO CON PARÁMETRO
+    private void MoveEnemy(Vector3 direction)
+    {
+        transform.Translate(speedEnemy * Time.deltaTime * direction);
     }
 }
--- CLASE 6 - INPUT/Assets/Scripts/PlayerController.cs
+++ CLASE 6 - INPUT/Assets/Scripts/PlayerController.cs
@@ -6,28 +6,50 @@ public class PlayerController : MonoBehaviour
 {
     public int lifePlayer = 3;
     public string namePlayer = "Mr. Blue";
-    public float speedPlayer = 100.0f;
+    public float speedPlayer = 1f;
     public GameObject swordPlayer;
     public Vector3 initPosition = new Vector3(4, 2, 1);
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("EN EL FRAME INICIAL...");
         transform.position = initPosition;
-        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        Debug.Assert(lifePlayer > 0);
         swordPlayer.GetComponent<SwordController>().SetSwordName("Espadon 9000");
-        //Debug.Log(swordPlayer.GetComponent<SwordController>().nameSword);
-        Debug.Log(swordPlayer.GetComponent<SwordController>().GetSwordName());
-        //swordPlayer.transform.position = transform.position + new Vector3(0,0,1);
-        swordPlayer.transform.position   = transform.position + Vector3.forward;
+        swordPlayer.transform.position = transform.position + Vector3.forward;
         swordPlayer.transform.localScale = transform.localScale;
-        //Debug.Break();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("EN CADA FRAME...");
+        /* INPUT PRIMER EJEMPLO        
+        if (Input.GetKeyDown(KeyCode.W)){
+            //Debug.Log("ADELANTE");
+            MovePlayer(Vector3.left);
+        }
+        */
+        /* INPUT SEGUNDO EJEMPLO
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            //Debug.Log("ADELANTE");
+            MovePlayer(Vector3.left);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            MovePlayer(Vector3.right);
+        }*/
+
+        Move();
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        transform.Translate(speedPlayer * Time.deltaTime * direction);
+    }
+
+    private void Move()
+    {
+        float ejeHorizontal = Input.GetAxisRaw("Horizontal");
+        float ejeVertical = Input.GetAxisRaw("Vertical");
+        transform.Translate(speedPlayer * Time.deltaTime * new Vector3(-ejeVertical, 0, ejeHorizontal));
     }
 }
